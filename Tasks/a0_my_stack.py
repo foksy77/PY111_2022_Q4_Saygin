4fdbb00079d2b1090533f74b5d0e2c024cbd6f4e
Sergey <79013082619.s@gmail.com>
2022-09-02 20:38:35 +0300

    a0 a1 done a2 init

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -4,41 +4,52 @@ My little Stack
 from typing import Any
 
 
-def push(elem: Any) -> None:
-    """
-    Operation that add element to stack
+class Stack:
+    def __init__(self):
+        self.__my_stack = []
 
-    :param elem: element to be pushed
-    :return: Nothing
-    """
-    print(elem)
-    return None
+    def push(self, elem: Any) -> None:
+        """
+        Operation that add element to stack
 
+        :param elem: element to be pushed
+        :return: Nothing
+        """
+        self.__my_stack.append(elem)
+        # print(elem)
+        return None
 
-def pop() -> Any:
-    """
-    Pop element from the top of the stack. If not elements - should return None.
+    def pop(self) -> Any:
+        """
+        Pop element from the top of the stack. If not elements - should return None.
 
-    :return: popped element
-    """
-    return None
+        :return: popped element
+        """
 
+        if len(self.__my_stack) == 0:
+            return None
 
-def peek(ind: int = 0) -> Any:
-    """
-    Allow you to see at the element in the stack without popping it.
+        return self.__my_stack.pop()
 
-    :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
-    :return: peeked element or None if no element in this place
-    """
-    print(ind)
-    return None
+    def peek(self, ind: int = 0) -> Any:
+        """
+        Allow you to see at the element in the stack without popping it.
 
+        :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
+        :return: peeked element or None if no element in this place
+        """
+        # print(ind)
+        if ind >= len(self.__my_stack):
+            return None
 
-def clear() -> None:
-    """
-    Clear my stack
+        return self.__my_stack[-1 - ind]
 
-    :return: None
-    """
-    return None
+    def clear(self) -> None:
+        """
+        Clear my stack
+
+        :return: None
+        """
+        self.__my_stack.clear()
+
+        return None
--- Tasks/a1_my_queue.py
+++ Tasks/a1_my_queue.py
@@ -4,41 +4,46 @@ My little Queue
 from typing import Any
 
 
-def enqueue(elem: Any) -> None:
-    """
-    Operation that add element to the end of the queue
+class Queue:
+    def __init__(self):
+        self.__my_queue = []
 
-    :param elem: element to be added
-    :return: Nothing
-    """
-    print(elem)
-    return None
+    def enqueue(self, elem: Any) -> None:
+        """
+        Operation that add element to the end of the queue
+        :param elem: element to be added
+        :return: Nothing
+        """
+        self.__my_queue.append(elem)
+        print(elem)
+        return None
 
+    def dequeue(self) -> Any:
+        """
+        Return element from the beginning of the queue. Should return None if no elements.
+        :return: dequeued element
+        """
+        if len(self.__my_queue) == 0:
+            return None
 
-def dequeue() -> Any:
-    """
-    Return element from the beginning of the queue. Should return None if no elements.
+        return self.__my_queue.pop(0)
 
-    :return: dequeued element
-    """
-    return None
+    def peek(self, ind: int = 0) -> Any:
+        """
+        Allow you to see at the element in the queue without dequeuing it
+        :param ind: index of element (count from the beginning)
+        :return: peeked element
+        """
+        print(ind)
+        if ind >= len(self.__my_queue):
+            return None
 
+        return self.__my_queue[ind]
 
-def peek(ind: int = 0) -> Any:
-    """
-    Allow you to see at the element in the queue without dequeuing it
-
-    :param ind: index of element (count from the beginning)
-    :return: peeked element
-    """
-    print(ind)
-    return None
-
-
-def clear() -> None:
-    """
-    Clear my queue
-
-    :return: None
-    """
-    return None
+    def clear(self) -> None:
+        """
+        Clear my queue
+        :return: None
+        """
+        self.__my_queue.clear()
+        return None
